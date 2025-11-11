93a2efc60bd1d551b2cca2a9c6ffb9e461441ef5
Luís Miguel <luismiguelpessoal1@gmail.com>
2025-11-11 18:51:54 -0300

    Inserindo as notas no vetor e calculando a média

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ var situacoes = [];
 
 var aprovados = 0;
 var reprovados = 0;
+
+for (i = 0; i < quantAlunos; i++) {
+    var notaAv1 = parseFloat(prompt("Informe a nota da 1a avaliação do "+ (i + 1) +"o aluno: "));
+    var notaAv2 = parseFloat(prompt("Informe a nota da 2a avaliação do "+ (i + 1) +"o aluno: "));
+
+    notasAv1.push(notaAv1);
+    notasAv2.push(notaAv2);
+
+    var media = ((notaAv1 + notaAv2) / 2);
+    medias.push(media);
+}
