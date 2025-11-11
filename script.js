9dc7c7a0abde92340d3719a46c9f12db44117f7a
Luís Miguel <luismiguelpessoal1@gmail.com>
2025-11-11 18:47:09 -0300

    Variáveis inicializadas

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 var quantAlunos = parseInt(prompt("Informe a quantidade de alunos: "))
+
+var notasAv1 = [];
+var notasAv2 = [];
+var medias = [];
+var situacoes = [];
+
+var aprovados = 0;
+var reprovados = 0;
